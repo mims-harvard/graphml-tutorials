caba1ffe7bb4953a5ba9f5d44fd3d6551df0e95e
minjeancho <57046192+minjeancho@users.noreply.github.com>
2020-07-30 12:33:21 -0400

    added molecule.py

--- 03-graph-classification/molecule.py
+++ 03-graph-classification/molecule.py
@@ -1,9 +1,9 @@
 class Molecule:
     """
-	:param nodes: list of integers of atomic number of the molecule 
-    :param label: if task is property prediction task, label is {1,0}
-    if task is multi regreession task, label is list of three topological indices, 
-    [wiener_idx, hyper_wiener_idx, zagreb_idx]
+    	:param nodes: list of integers of atomic number of the molecule 
+        :param label: if task is property prediction task, label is {1,0}
+        if task is multi regreession task, label is list of three topological indices, 
+        [wiener_idx, hyper_wiener_idx, zagreb_idx]
     """
     
     def __init__(self, nodes, label, am):
